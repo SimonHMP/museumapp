c4e2efc048bd232272dc7ffc0aea8bb064ad8b94
SimonHMP <simon.hempelman@gmail.com>
2019-11-07 12:10:38 +0100

    comment section finalized :D

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,26 +1,78 @@
 
-
+/*
 function submitComment() {
 
-    const inputField = document.getElementById('name')
+
+
+    event.preventDefault()  //stop page from refreshing on click
+
+    const inputField = document.getElementById("name")
     const name = inputField.value
 
-    const inputFieldOne = document.getElementById('msg')
+    const inputFieldOne = document.getElementById("msg")
     const msg = inputFieldOne.value
 
 
-    const comment = document.createElement('section')
-    const h3 = document.createElement('h3') 
-    const p = document.createElement('p')
+    const comment = document.createElement("section")
+    const h3 = document.createElement("h3") 
+    const p = document.createElement("p")
     
-    h3.innerHTML = '${name} said:';
+    h3.innerHTML =  '${name} said:';
     p.innerHTML = msg;
-    comment.classList.add('comment');
+    comment.classList.add("comment");
     comment.appendChild(h3);
-    comment.appendChild(p)
+    comment.appendChild(p);
 
+    const commentSection = document.getElementById("comments");
+    commentSection.appendChild(comment);
+    
+
+
+    inputField.value = null;
+    inputFieldOne.value = null;
+    
+    }
+*/
+
+// add event handler
+function submitComment() {
+    // gather data
+
+    event.preventDefault()  //stop page from refreshing on click
+    const inputField = document.getElementById("name");
+    const name = inputField.value;
+    const textArea = document.getElementById("msg");
+    const msg = textArea.value;
+  
+    // create the elements you need
+    const comment = document.createElement("section");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+  
+    // adjust the elements we created
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add("comment");
+    comment.appendChild(h3);
+    comment.appendChild(p);
+  
+    // display the elements on the page
+    const commentSection = document.getElementById("comments");
+    commentSection.appendChild(comment);
+  
+    // reset form values
 
     
+    inputField.value = null;
+    textArea.value = null;
+
+    // some console logs
+
     console.log(comment)
-}
+    //console.log(h3)
+    //console.log(name)
+    
+
+    
+  }
 
