cb14d0ba7ea5756d87fe3aa2384c6230c231a6c9
SimonHMP <simon.hempelman@gmail.com>
2019-11-07 14:59:27 +0100

    added restrictions to comment form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -35,15 +35,57 @@ function submitComment() {
 */
 
 // add event handler
+
+function doesNotPassAllValidations(name, msg) {
+    key = event.keyCode;
+    if (!name || !msg) {
+        alert('You forgot to fill in your name or message!')
+        return true
+       
+    } 
+    if (msg.length > 25 || name.length > 25) {
+        alert('Your message is too long man')
+        return true 
+    } else {
+        return false
+    }}
+    
+
+
+
+function capitalizeFirstLetter(msg) {
+    let restOfString = msg.slice(1, msg.length)
+    let firstLetter = msg.slice(0,1)
+    firstLetter = firstLetter.toUpperCase()
+   
+    msg = firstLetter + restOfString
+    return msg
+}
+    
+
+
+
+
+
+
+
 function submitComment() {
     // gather data
 
+
     event.preventDefault()  //stop page from refreshing on click
+
     const inputField = document.getElementById("name");
-    const name = inputField.value;
+    let name = inputField.value;
     const textArea = document.getElementById("msg");
     const msg = textArea.value;
   
+    if(doesNotPassAllValidations(name, msg)) {
+        return null
+    } else {
+
+    name = capitalizeFirstLetter(name)
+    
     // create the elements you need
     const comment = document.createElement("section");
     const h3 = document.createElement("h3");
@@ -68,11 +110,15 @@ function submitComment() {
 
     // some console logs
 
-    console.log(comment)
-    //console.log(h3)
-    //console.log(name)
-    
+  
+    }
 
-    
-  }
+
+}
+  
+
+
+
+
+
 
